d3d0b5ada1e2695711da3df510bef89843102ddd
“Arthur-Bamberg” <bamberguisses@gmail.com>
2024-05-30 20:21:31 -0300

    feat: criação do tipo pet

--- src/Controller/PetController.ts
+++ src/Controller/PetController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
+import type TipoPet from "../tipos/TipoPet";
 
-let listaDePets = [];
+let listaDePets: TipoPet[] = [];
 
 export default class PetController {
   criaPet(req: Request, res: Response) {
-    const novoPet = req.body;
+    const { id, nome, especie, adotado, idade } = <TipoPet>req.body;
+    const novoPet: TipoPet = { id, nome, especie, adotado, idade };
     listaDePets.push(novoPet);
     return res.status(201).json(novoPet);
   }
