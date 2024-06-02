7a12b323da6f74c0c1cd59b28a6eab1ddd6c13e1
“Arthur-Bamberg” <bamberguisses@gmail.com>
2024-06-02 12:13:27 -0300

    feat: finalização do CRUD de adotantes

--- src/repositories/interfaces/InterfaceAdotanteRepository.ts
+++ src/repositories/interfaces/InterfaceAdotanteRepository.ts
@@ -2,4 +2,14 @@ import AdotanteEntity from "../../entities/AdotanteEntity";
 
 export default interface InterfaceAdotanteRepository {
   criaAdotante(adotante: AdotanteEntity): void | Promise<void>;
+  listaAdotantes(): AdotanteEntity[] | Promise<AdotanteEntity[]>;
+
+  atualizaAdotante(
+    id: number,
+    adotante: AdotanteEntity,
+  ): Promise<{ success: boolean; message?: string }> | void;
+
+  deletaAdotante(
+    id: number,
+  ): Promise<{ success: boolean; message?: string }> | void;
 }
