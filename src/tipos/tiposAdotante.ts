651d3a248b6612d9cb9d38cbfe5b7b7dd48fea8b
“Arthur-Bamberg” <bamberguisses@gmail.com>
2024-06-26 21:14:34 -0300

    feat: DTOs de adotante

--- src/tipos/tiposAdotante.ts
+++ src/tipos/tiposAdotante.ts
@@ -2,6 +2,11 @@ import AdotanteEntity from "../entities/AdotanteEntity";
 
 export type TipoRequestBodyAdotante = Omit<AdotanteEntity, "id">;
 
+export type TipoRequestParamsAdotante = { id?: string };
+
 export type TipoResponseBodyAdotante = {
-  data?: Pick<AdotanteEntity, "id" | "nome" | "celular">;
+  data?:
+    | Pick<AdotanteEntity, "id" | "nome" | "celular">
+    | Pick<AdotanteEntity, "id" | "nome" | "celular">[];
+  error?: unknown;
 };
